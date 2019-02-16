4ab118bc57ff03ff006f06d144d3e4b82f2f70c4
heyx <1064042411>
2019-02-16 17:26:42 +0800

    Initial commit

---
+++ src/main/java/com/heyx/javacv/JavacvApplication.java
@@ -0,0 +1,14 @@
+package com.heyx.javacv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavacvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavacvApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/heyx/javacv/JavacvApplicationTests.java
@@ -0,0 +1,17 @@
+package com.heyx.javacv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JavacvApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
